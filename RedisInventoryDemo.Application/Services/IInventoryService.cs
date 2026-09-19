25d61a31fe17f7d9017b4919d86b4d1da8982f2b
Marcela <mgonzalez@puntoticket.cl>
2026-09-19 13:13:08 -0300

    add inventory application service

---
+++ RedisInventoryDemo.Application/Services/IInventoryService.cs
@@ -0,0 +1,8 @@
+﻿using RedisInventoryDemo.Domain.Entities;
+
+namespace RedisInventoryDemo.Application.Services;
+
+public interface IInventoryService
+{
+    Task<InventoryItem?> GetByIdAsync(int id);
+}
---
+++ RedisInventoryDemo.Application/Services/InventoryService.cs
@@ -0,0 +1,19 @@
+﻿using RedisInventoryDemo.Application.Abstractions.Persistence;
+using RedisInventoryDemo.Domain.Entities;
+
+namespace RedisInventoryDemo.Application.Services;
+
+public sealed class InventoryService : IInventoryService
+{
+    private readonly IInventoryRepository _inventoryRepository;
+
+    public InventoryService(IInventoryRepository inventoryRepository)
+    {
+        _inventoryRepository = inventoryRepository;
+    }
+
+    public async Task<InventoryItem?> GetByIdAsync(int id)
+    {
+        return await _inventoryRepository.GetByIdAsync(id);
+    }
+}
