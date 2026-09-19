2b23664c954a244695eb92a65ca2a52dde821dd7
Marcela <mgonzalez@puntoticket.cl>
2026-09-19 13:07:58 -0300

    add inventory repository abstraction

---
+++ RedisInventoryDemo.Application/Abstractions/Persistence/IInventoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using RedisInventoryDemo.Domain.Entities;
+
+namespace RedisInventoryDemo.Application.Abstractions.Persistence;
+
+public interface IInventoryRepository
+{
+    Task<InventoryItem?> GetByIdAsync(int id);
+}
