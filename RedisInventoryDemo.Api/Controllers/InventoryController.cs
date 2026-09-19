31c3ddff189de28c71b1b248939f79b9b2452d86
Marcela <mgonzalez@puntoticket.cl>
2026-09-19 13:20:30 -0300

    feat: add inventory get endpoint

---
+++ RedisInventoryDemo.Api/Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisInventoryDemo.Application.Services;
+using RedisInventoryDemo.Domain.Entities;
+
+namespace RedisInventoryDemo.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class InventoryController : ControllerBase
+{
+    private readonly IInventoryService _inventoryService;
+
+    public InventoryController(IInventoryService inventoryService)
+    {
+        _inventoryService = inventoryService;
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(InventoryItem), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<InventoryItem>> GetById(int id)
+    {
+        var item = await _inventoryService.GetByIdAsync(id);
+
+        if (item is null)
+            return NotFound();
+
+        return Ok(item);
+    }
+}
