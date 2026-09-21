2633e0a07fff596dc5670ca88403c6f83dc39c51
Marcela <mgonzalez@puntoticket.cl>
2026-09-21 10:31:37 -0300

    feat: configure redis connection

--- RedisInventoryDemo.Infrastructure/DependencyInjection.cs
+++ RedisInventoryDemo.Infrastructure/DependencyInjection.cs
@@ -1,15 +1,36 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using RedisInventoryDemo.Application.Abstractions.Persistence;
 using RedisInventoryDemo.Application.Services;
+using RedisInventoryDemo.Infrastructure.Configuration;
 using RedisInventoryDemo.Infrastructure.Persistence;
+using StackExchange.Redis;
+
+
 
 namespace RedisInventoryDemo.Infrastructure;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(
-        this IServiceCollection services)
+        this IServiceCollection services,  IConfiguration configuration)
     {
+
+        services.Configure<RedisOptions>(
+          configuration.GetSection(RedisOptions.SectionName));
+
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var options = sp
+                .GetRequiredService<IOptions<RedisOptions>>()
+                .Value;
+
+            return ConnectionMultiplexer.Connect(
+                options.ConnectionString);
+        });
+
+
         services.AddScoped<IInventoryRepository, InMemoryInventoryRepository>();
         services.AddScoped<IInventoryService, InventoryService>();
 
